12eb17771cbbaf1c4fff8015edba66bc0088a714
Shivam Mathur <shivam_jpr@hotmail.com>
2024-03-04 10:55:15 +0000

    Test

--- src/install.sh
+++ src/install.sh
@@ -15,10 +15,19 @@ install() {
   [ "$debug" = 'true' ] && SUFFIX='-debug'
   [ "$ts" = 'true' ] && SUFFIX="$SUFFIX-zts"
 
+  BREW_PREFIX="$(brew --prefix)"
+
   tar_file=php@"$version$SUFFIX".tar.zst
   get /tmp/"$tar_file" "https://github.com/shivammathur/php-darwin/releases/latest/download/$tar_file"
   zstd -dq --force /tmp/"$tar_file"
-  sudo tar xf /tmp/php@"$version$SUFFIX".tar -C "$(brew --prefix)"
+  sudo rm -rf "$BREW_PREFIX"/share/gettext/its
+  sudo tar xf /tmp/php@"$version$SUFFIX".tar -C "$BREW_PREFIX"
+  # if command -v gtar 2>/dev/null;
+  #   sudo gtar --overwrite -xf /tmp/php@"$version$SUFFIX".tar -C "$BREW_PREFIX"
+  # else
+  #   sudo rm -rf "$BREW_PREFIX"/share/gettext/its
+  #   sudo tar xf /tmp/php@"$version$SUFFIX".tar -C "$BREW_PREFIX"
+  # fi  
 }
 
 version=$1
